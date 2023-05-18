5843e19aedaba1329c332ccc104f3f825feddd24
mzaini30 <muhzaini30@gmail.com>
2023-05-18 23:05:13 +0800

    Upload dari laptop

--- src/App.svelte
+++ src/App.svelte
@@ -6,9 +6,25 @@
   import "warna-tailwind/warna.css";
   import "./App.css";
   import Wa from "./komponen/wa.svelte";
+
+  let html_iklan = "";
+
+  if (localStorage.html_iklan) {
+    html_iklan = localStorage.html_iklan;
+  }
+
+  async function muat_iklan() {
+    let data = await fetch(
+      "https://raw.githubusercontent.com/mzaini30/iklan/master/organize-jobs.html"
+    );
+    data = await data.text();
+    html_iklan = data;
+    localStorage.html_iklan = data;
+  }
+  muat_iklan();
 </script>
 
-<div class="mb-14">
+<div class="">
   <Router {routes} />
-  <Wa />
+  {@html html_iklan}
 </div>
